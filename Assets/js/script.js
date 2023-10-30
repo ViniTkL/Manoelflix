7b4c468716cedeb2953a44c6317c21a16269d16a
ViniTkL <vinimaxter150@gmail.com>
2023-10-30 16:42:51 -0400

    JS e Musicas adicionado >:)

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('.aviso-btn');
+
+btn.addEventListener('click',()=>{
+    var bagulho = document.querySelector('.aviso');
+
+    bagulho.style.display = 'none';
+
+    console.log('sexo');
+});
+
