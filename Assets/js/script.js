cc3cc944168374ba017aae95047340d27e88c4c8
ViniTkL <vinimaxter150@gmail.com>
2023-10-31 15:23:14 -0400

    Falta fazer o mobile :(

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,65 @@
 var btn = document.querySelector('.aviso-btn');
 
 btn.addEventListener('click',()=>{
-    var bagulho = document.querySelector('.aviso');
+    var aviso = document.querySelector('.aviso');
 
-    bagulho.style.display = 'none';
+    aviso.style.display = 'none';
 
-    console.log('sexo');
 });
+
+
+
+var videos = document.getElementsByClassName('video-content');
+
+videos[0].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video1');
+
+    link.style.display = 'flex';
+});
+
+
+videos[1].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video2');
+
+    link.style.display = 'flex';
+});
+
+
+videos[2].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video3');
+
+    link.style.display = 'flex';
+});
+
+videos[3].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video4');
+
+    link.style.display = 'flex';
+});
+
+videos[4].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video5');
+
+    link.style.display = 'flex';
+});
+
+videos[5].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video6');
+
+    link.style.display = 'flex';
+});
+
+videos[6].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video7');
+
+    link.style.display = 'flex';
+});
+
+videos[7].addEventListener('click', ()=>{
+    var link = document.querySelector('#link-video8');
+
+    link.style.display = 'flex';
+});
+
+
 
